ade20e88d0b3a834fcff5d3d1a983f861a6eefda
A. Rebecca O <detoun4med@gmail.com>
2018-02-17 23:07:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name){
+<<<<<<< HEAD
 return `Happy holidays, ${name}!`
 }
 
@@ -12,6 +13,15 @@ function happyHolidayTo(holiday, name){
 return `Happy ${holiday}, ${name}!`
 }
  
+=======
+   return 'Happy holidays, ${name}!'
+}
+
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!'
+}
+
+>>>>>>> f86a3329b6ae2767d7edd18c425eb1be60460097
 function holidayCountdown(holiday, days){
  return `It\'s ${days} days until ${holiday}!`
 }
